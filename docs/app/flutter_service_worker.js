06efc0945bdef7c0a58ada91cc8aa4c62bbe740f
Moulin <chovatiyamoulin62@gmail.com>
2026-06-29 20:17:34 +0530

    fix: made the Sold carat figure same as shown in the Sell Tab.

--- docs/app/flutter_service_worker.js
+++ docs/app/flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "25eae9ec6ecb2a95c61653f12f6f635c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "d41d8cd98f00b204e9800998ecf8427e",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "6316ee44dc9a4eff692a2f2d20e3cfcf",
+"flutter_bootstrap.js": "2d7729f764a89f506dcbf02a1d1bd5f8",
 "icons/Icon-192.png": "d41d8cd98f00b204e9800998ecf8427e",
 "icons/Icon-512.png": "d41d8cd98f00b204e9800998ecf8427e",
 "icons/Icon-maskable-192.png": "d41d8cd98f00b204e9800998ecf8427e",
 "icons/Icon-maskable-512.png": "d41d8cd98f00b204e9800998ecf8427e",
 "index.html": "48b58c3ac08b13300603546ba37599b7",
 "/": "48b58c3ac08b13300603546ba37599b7",
-"main.dart.js": "acc9e27aac3d2442501ec8ac0eaf2eb2",
+"main.dart.js": "8f440b9218d7e8016507f431a2c88852",
 "manifest.json": "0c07d714fda47419a901b98d2b602692",
 "version.json": "8a25b40d9c50b55255b9b74c94d3cf96"};
 // The application shell files that are downloaded before a service worker can
